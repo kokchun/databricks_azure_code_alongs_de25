07bcaa1c1858b587cb05eed29c0e93087edc4c51
kokchun <kokchungiang@gmail.com>
2026-05-21 10:28:48 +0000

    time stamp clean

--- 06_silver/utilities/utils.py
+++ 06_silver/utilities/utils.py
@@ -1,14 +1,9 @@
-from pyspark.sql.functions import udf
-from pyspark.sql.types import BooleanType
-import re
+import re 
 
-@udf(returnType=BooleanType())
-def is_valid_email(email):
-    """
-    This function checks if the given email address has a valid format using regex.
-    Returns True if valid, False otherwise.
-    """
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    if email is None:
-        return False
-    return re.match(pattern, email) is not None
+#     Customer   Email    -> customer_email
+def to_snake_case(name):
+    return re.sub(r"[\s]+", "_", name.strip().casefold())
+
+def rename_columns_to_snake_case(df):
+    new_columns = [to_snake_case(column) for column in df.columns]
+    return df.toDF(*new_columns)
